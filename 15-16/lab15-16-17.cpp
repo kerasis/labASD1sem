0ff2d5b33a99c349089c24f0699df33df99eb371
kseniya zaika <kseniya.zaika.2004@gmail.com>
2023-12-19 15:53:51 +0200

    деревья

---
+++ 15-16/lab15-16-17.cpp
@@ -0,0 +1,114 @@
+﻿#include <iostream>
+#include <vector>
+
+#include <sstream>
+#include <stack>
+
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+int f_index(const std::string s, int start_ind, int end_ind) //поиск индекса последней скобки левого поддерева
+{
+    if (start_ind > end_ind) { return -1; }
+    std::stack<char> a;
+    for (int i = start_ind; i <= end_ind; i++)
+    {
+        if (s[i] == '(') { a.push(s[i]); }
+        else if (s[i] == ')')
+        {
+            if (a.top() == '(')
+                a.pop();
+            if (a.empty()) { return i; }
+        }      
+    }
+    return -1;
+}
+
+Node* fillTree(const std::string s, int start_ind, int end_ind)
+{
+    if (start_ind > end_ind) { return nullptr; }
+    int val = 0;
+    while (start_ind <= end_ind and (s[start_ind]-'0') >= 0 and (s[start_ind]-'0') <= 9)
+    {
+        val *= 10;
+        val += s[start_ind]-'0';
+        start_ind += 1;
+    }
+
+    auto* root = new Node(val);
+    int ind = -1;
+    if (start_ind <= end_ind and s[start_ind] == '(') { ind = f_index(s, start_ind, end_ind); }
+
+    if (ind != -1)
+    {
+        root->left = fillTree(s, start_ind + 1, ind - 1);
+        root->right = fillTree(s, ind + 2, end_ind - 1);
+    }
+    return root;
+}
+void preOrder(Node* node) {
+    if (node == nullptr) { return; }
+    std::cout << node->data << " ";
+    preOrder(node->left);
+    preOrder(node->right);
+}
+void inOrder(Node* node) {
+    if (node == nullptr) { return; }
+    inOrder(node->left);
+    std::cout << node->data << " ";
+    inOrder(node->right);
+}
+
+void postOrder(Node* node) {
+    if (node == nullptr) { return; }
+    postOrder(node->left);
+    postOrder(node->right);
+    std::cout << node->data << " ";
+}
+
+void stackOrder(Node* node)
+{
+    if (node == nullptr) { return; }
+    std::stack<Node*> s;
+    s.push(node);
+
+    while (!s.empty())
+    {
+        auto temp = s.top();
+        std::cout << temp->data << " ";
+
+        s.pop();
+        if (temp->right != nullptr) { s.push(temp->right); }
+        if (temp->left != nullptr) { s.push(temp->left); }
+        
+    }
+}
+
+int main() {
+   
+    std::string str = "0(3(1)(6(4)(7)))(10(14)(13))";
+    Node* root  = fillTree(str, 0, str.length() - 1);
+
+    std::cout << "preOrder: ";
+    preOrder(root);
+    std::cout << std::endl;
+
+    std::cout << "inOrder: ";
+    inOrder(root);
+    std::cout << std::endl;
+
+    std::cout << "postOrder: ";
+    postOrder(root);
+    std::cout << std::endl;
+
+    std::cout << "stackOrder: ";
+    stackOrder(root);
+    std::cout << std::endl;
+}
+
---
+++ 17/lab17.cpp
@@ -0,0 +1,163 @@
+﻿#include <iostream>
+#include <string>
+struct Node 
+{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) : data(value), left(nullptr), right(nullptr) {}
+};
+
+class bnp
+{
+private:
+    Node* root;
+    std::string brNot(Node* node) 
+    {
+        if (node == nullptr) { return ""; }
+        std::string result = std::to_string(node->data);
+
+        if (node->left != nullptr || node->right != nullptr) 
+        {
+            if (node->left != nullptr)
+            {
+                result += "(" + brNot(node->left) + ")";
+            }
+            //result += "(" + brNot(node->left) + ")";
+            if (node->right != nullptr) 
+            {
+                result += "(" + brNot(node->right) + ")";
+            }
+        }
+        return result;
+    }
+
+    Node* ins(Node* node, int val)
+    {
+        if (node == nullptr) { return new Node(val); }
+        if (val < node->data) { node->left = ins(node->left, val);  }
+        else if (val > node->data) { node->right = ins(node->right, val); }
+        return node;
+    }
+
+    Node* fMin(Node* node)
+    {
+        while (node->left != nullptr) { node = node->left; }
+        return node;
+    }
+    Node* fMax(Node* node)
+    {
+        while (node->right != nullptr) { node = node->right; }
+        return node;
+    }
+
+    bool searchRec(Node* node, int value) {
+        if (node == nullptr) { return false; }
+        if (value == node->data) {  return true; }
+
+        else if (value < node->data) 
+        {
+            return searchRec(node->left, value);
+        }
+        else
+        {
+            return searchRec(node->right, value);
+        }
+    }
+
+    Node* rem(Node* node, int value) {
+        if (node == nullptr) { return node; }
+
+        if (value < node->data)
+        {
+            node->left = rem(node->left, value);
+        }
+        else if (value > node->data)
+        {
+            node->right = rem(node->right, value);
+        }
+        else 
+        {
+            if (node->left == nullptr) 
+            {
+                Node* temp = node->right;
+                delete node;
+                return temp;
+            }
+            else if (node->right == nullptr) 
+            {
+                Node* temp = node->left;
+                delete node;
+                return temp;
+            }
+
+            Node* temp = fMin(node->right);
+            node->data = temp->data;
+            node->right = rem(node->right, temp->data);
+        }
+        return node;
+    }
+
+public:
+    bnp() : root(nullptr) {};
+
+    void insert(int value)
+    {
+        root = ins(root, value);
+    }
+    int findMin()
+    {
+        return fMin(root)->data;
+    }
+    int findMax()
+    {
+        return fMax(root)->data;
+    }
+    void remove(int val)
+    {
+        root = rem(root, val);
+    }
+    bool search(int value) 
+    {
+        return searchRec(root, value);
+    }
+    void printTree()
+    {
+        std::cout << "Tree: " << brNot(root) << std::endl;
+    }
+
+};
+
+
+int main()
+{
+    bnp tree;
+    std::string s = "0(3(1)(6(7)(4)))(10(14)(13))";
+    int ind = 0;
+    while (ind < s.length())
+    {
+        bool flag = false;
+        int val = 0;
+        while ((s[ind] - '0') >= 0 and (s[ind] - '0') <= 9)
+        {
+            flag = true;
+            val *= 10;
+            val += s[ind] - '0';
+            ind += 1;
+        }
+        if (flag)
+        {
+            tree.insert(val);
+        }
+        
+        ind += 1;
+    }
+
+    std::cout << "Max: " << tree.findMax() << " min: " << tree.findMin() << " 1?: " << tree.search(1) << std::endl;
+    tree.remove(1);
+    std::cout << " 1?: " << tree.search(1) << std::endl;
+    tree.printTree();
+    tree.remove(0);
+    tree.printTree();
+}
